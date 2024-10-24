c02e4c8667f6d4fea5d92340998aff64a46e3dc5
tiennehihi <vuductientienti@gmail.com>
2024-10-24 23:03:47 +0700

    update json-server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('data.json'); // Chỉ ra file JSON của bạn
+const router = jsonServer.router('data.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running at: ', process.env.PORT || 3000);
 });
